810b2731133f7203e0acf07baa1c3f33d789eaa6
pooh-polo <pooh.polo1220@gmail.com>
2026-06-23 01:58:41 +0900

    Fix base to /mona-page/

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: './',
-  // base: '/mona-page/',
+  base: '/mona-page/', 
 });
